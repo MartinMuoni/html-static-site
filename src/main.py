6437edc5d62e96007cdf65f3fea0af0b07890d33
MartinMuoni <martinmuoni@gmail.com>
2026-01-27 18:50:31 +0200

    Add clarifying comment to main entry point

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from textnode import TextNode, TextType
 
+# Entry point for HTML static site generator
+
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
